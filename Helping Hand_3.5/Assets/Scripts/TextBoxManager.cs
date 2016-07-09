e6853feb1bb7dfdde215b4fc8496aef412d3ec4b
Andrew de Neve <Andrew de Neve>
2016-07-09 16:50:56 -0700

    working on dialog box

---
+++ Helping Hand_3.5/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour
+{
+    public GameObject TextBox;
+
+    public Text theText;
+    public TextAsset textfile;
+    public string[] textlines;
+
+    public int currentline;
+    public int endatline;
+
+    public PlayerManager player;
+    // Use this for initialization
+    void Start()
+    {
+        
+        if (textfile != null)
+        {
+            textlines = (textfile.text.Split('\n'));
+        }
+
+        if(endatline == 0)
+        {
+            endatline = textlines.Length - 1;
+        }
+
+    }
+    void Update()
+    {
+        theText.text = textlines[currentline];
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            currentline = currentline + 1;
+        }
+        if (currentline > endatline)
+        {
+            TextBox.SetActive(false);
+            currentline = 0;
+        }
+    }
+}
